1e396db60ffd70c3d961f162e2ba6dd71567b22e
Dragos Tutui <dragos.tutui@intelligentbee.com>
2022-07-20 14:02:28 +0300

    first test

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var http = require("http");
-
+var CoinGecko = require("coingecko-api")
+const CoinGeckoClient = new CoinGecko();
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World. This page is running Node.js version: ");
